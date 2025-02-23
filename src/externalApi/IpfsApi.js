4ce24270f63d3c06ebbfa33a3e26bd0da1b6cd37
Vivek Sahu <viveksahu1762@gmail.com>
2025-02-23 16:11:26 +0530

    changed pinata ipfs link

--- src/externalApi/IpfsApi.js
+++ src/externalApi/IpfsApi.js
@@ -1,6 +1,6 @@
 // externalApi/lpfsApi.js
 
-const axios = require('axios');
+const axios = require("axios");
 
 /**
  * Fetch metadata from IPFS using a public gateway.
@@ -10,15 +10,15 @@ const axios = require('axios');
 async function fetchMetadataFromIpfs(cid) {
   try {
     // Construct the URL for a public IPFS gateway
-    const url = `https://harlequin-secure-tortoise-165.mypinata.cloud/ipfs/${cid}`;
+    const url = `https://gateway.pinata.cloud/ipfs/${cid}`;
     const response = await axios.get(url);
     return response.data; // Return the fetched metadata
   } catch (error) {
-    console.error('Failed to fetch data from IPFS:', error.message);
+    console.error("Failed to fetch data from IPFS:", error.message);
     throw error;
   }
 }
 
 module.exports = {
-  fetchMetadataFromIpfs
+  fetchMetadataFromIpfs,
 };
